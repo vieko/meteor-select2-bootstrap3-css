228daeea64e9b9c3ce7a205f4276eb458a8542cb
Vieko Franetovic <vieko.franetovic@gmail.com>
2015-09-22 08:27:41 -0600

    bumped select2 to 4.0.0

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   summary: 'CSS to make Select2 fit in with Bootstrap 3',
-  name: 'zimme:select2-bootstrap3-css',
+  name: 'metabus:select2-bootstrap3-css',
   git: 'https://github.com/zimme/meteor-select2-bootstrap3-css.git',
-  version: '1.4.6'
+  version: '1.4.6',
 });
 
 Package.onUse(function (api) {
@@ -10,10 +10,10 @@ Package.onUse(function (api) {
 
   api.use([
     'mizzao:bootstrap-3@3.2.0',
-    'natestrauser:select2@3.4.7',
+    'natestrauser:select2@4.0.0',
     'nemo64:bootstrap@3.3.4_1',
     'simison:bootstrap3-less@0.3.0',
-    'twbs:bootstrap@3.3.4'
+    'twbs:bootstrap@3.3.4',
   ], 'client', {weak: true});
 
   api.addFiles('select2-bootstrap-css/select2-bootstrap.css', 'client');
